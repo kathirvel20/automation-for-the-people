b619cbb6593d13fe7525c0346546b4f7df95fff3
kathirvel20 <42608229+kathirvel20@users.noreply.github.com>
2018-09-24 12:03:05 +0530

    region change

--- infrastructure/init.sh
+++ infrastructure/init.sh
@@ -6,4 +6,4 @@ read GITHUB_USER_NAME
 echo "enter a personal GitHub access token. Generate one here: https://github.com/settings/tokens (scopes needed: admin:repo_hook, repo)"
 read GITHUB_OAUTH_TOKEN
 
-aws --region eu-west-1 cloudformation create-stack --stack-name "aftp-pipeline" --template-body file://pipeline.yaml --parameters "ParameterKey=GitHubUserName,ParameterValue=${GITHUB_USER_NAME}" "ParameterKey=GitHubOAuthToken,ParameterValue=${GITHUB_OAUTH_TOKEN}" --capabilities CAPABILITY_IAM
+aws --region us-east-1 cloudformation create-stack --stack-name "aftp-pipeline" --template-body file://pipeline.yaml --parameters "ParameterKey=GitHubUserName,ParameterValue=${GITHUB_USER_NAME}" "ParameterKey=GitHubOAuthToken,ParameterValue=${GITHUB_OAUTH_TOKEN}" --capabilities CAPABILITY_IAM
